b81e701a9da18181f7b06e4f1a4c550dbc1659da
mldeanha <mldeanha@mtu.edu>
2017-02-07 17:17:44 -0500

    Added Bullets
    Added a Bullet class which contains bullet logic. Changed code in
    CircleShooter to add playerowned bullets and collisions between bullets
    and asteroids.

--- src/CircleShooter.java
+++ src/CircleShooter.java
@@ -17,113 +17,133 @@ import Enemy.*;
 import Utility.*;
 
 public class CircleShooter extends Game{
-	
+
 	final int c_size = 500;	//The size of the circle
 	int lives = 3;
-	
+
 	int[][] list = {{eType.ASTEROID.ordinal(),1000},
-					{eType.SHIP.ordinal(),0}};
-	
+			{eType.SHIP.ordinal(),0}};
+
 	Round r1 = new Round(500,15,1500,list, new Point((WIDTH/2), (HEIGHT/2)),c_size/2);
-	Player player = new Player( (WIDTH / 2), (HEIGHT/2), c_size/2);
-	
+	//x origin of circle, y origin of circle, radius of circle, game reference//
+	Player player = new Player( (WIDTH / 2), (HEIGHT/2), c_size/2 );
+
 	//Game Data
 	ArrayList<Enemy> enemies = new ArrayList<Enemy>();
+	public ArrayList<Bullet> playerBullets = new ArrayList<Bullet>();
 	boolean roundOver = false;
 	boolean gameOver = false;
-	
+
 	/**
 	 * Constructor of the game
 	 */
 	public CircleShooter(){
 	}
-	
+
 	@Override
 	public void tick(Graphics2D g, Input input, Sound sound) {	
 		if(!this.checkEndGame()){
-		//******************************************************************
-		// Game Calculations
-		//******************************************************************
+			//******************************************************************
+			// Game Calculations
+			//******************************************************************
 
-		//Move Player
-		player.updatePos( input );
-		
-		//Draw and move Enemies
-		ArrayList<Enemy> dead = new ArrayList<>();
-		for(Enemy e: enemies){
-			//e.setGame(this);
-			if(e instanceof Asteroid){
-				e.update(g);
-			}else if(e instanceof Ship){
-				g.setColor(Color.white);
-				g.fillRect(e.x, e.y, 5, 5);
+			//Move Player & get new Bullets
+			Bullet nullCheck = player.updatePos( input );
+			if( nullCheck != null ) playerBullets.add( nullCheck );
+
+			ArrayList<Bullet> spent = new ArrayList<>();
+			for(Bullet b: playerBullets){
+				b.update();
+				if(!b.getAlive()){
+					spent.add(b);
+				}
 			}
-			if (!e.alive) {
-				dead.add(e);
+
+			//Draw and move Enemies
+			ArrayList<Enemy> dead = new ArrayList<>();
+			for(Enemy e: enemies){
+				//e.setGame(this);
+				if(e instanceof Asteroid){
+					e.update(g);
+				}else if(e instanceof Ship){
+					g.setColor(Color.white);
+					g.fillRect(e.x, e.y, 5, 5);
+				}
+				if (!e.alive) {
+					dead.add(e);
+				}
+
+				//Check for collision
+				if(Calc.collide(new Point(player.getX(),player.getY()), player.getSize(), new Point(e.x,e.y), e.getSize())){
+					lives--;
+					dead.add(e);
+					//Player respawn [will need to be discussed]
+				}
+				for( Bullet b: playerBullets ){
+					if(Calc.collide(new Point(b.getX(),b.getY()), b.getSize(), new Point(e.x,e.y), e.getSize())){
+						dead.add(e);
+						spent.add(b);
+					}
+				}
+
+			}
+
+			// Remove dead bullets
+			playerBullets.removeAll(spent);
+			// Remove dead enemies
+			enemies.removeAll(dead);
+
+			//Check if the round is over
+			roundOver = r1.checkEndRound();
+
+			//Spawn new enemies
+			Enemy temp = r1.spawnEnemy();
+			if(temp!=null){
+				enemies.add(temp);
+				enemies.get(enemies.size()-1).setGame(this);
+			}
+
+
+			//******************************************************************
+			// Drawing
+			//******************************************************************
+			//Clear the screen (Black)  
+			g.setColor(Color.black);  
+			g.fillRect(0,0,WIDTH,HEIGHT);
+
+			//Draw lives
+			g.setColor(Color.red);
+			g.setFont(new Font("TimesRoman", Font.PLAIN, 24));
+			g.drawString("LIVES: "+lives, WIDTH-125, 25);
+
+			//Draw the Circle
+			g.setColor(Color.white);
+			//g.drawOval(WIDTH-550, 25, 500, 500);
+			g.drawOval((WIDTH / 2) - c_size/2, (HEIGHT/2) - c_size/2, c_size, c_size);
+
+			//Draw the Player
+			player.draw(g);
+
+			for(Bullet b: playerBullets){
+				b.draw(g);
 			}
 			
-			//Check for collision
-			if(Calc.collide(new Point(player.getX(),player.getY()), player.getSize(), new Point(e.x,e.y), e.getSize())){
-				lives--;
-				dead.add(e);
-				//Player respawn [will need to be discussed]
+			//Draw the enemies
+			for(Enemy e:enemies){
+				e.draw(g);
 			}
-			
-		}
-		
-		// Remove dead enemies
-		enemies.removeAll(dead);
-		
-		//Check if the round is over
-		roundOver = r1.checkEndRound();
-		
-		//Spawn new enemies
-		Enemy temp = r1.spawnEnemy();
-		if(temp!=null){
-			enemies.add(temp);
-			enemies.get(enemies.size()-1).setGame(this);
-		}
-		
-		
-		//******************************************************************
-		// Drawing
-		//******************************************************************
-		//Clear the screen (Black)  
-		g.setColor(Color.black);  
-		g.fillRect(0,0,WIDTH,HEIGHT);
-		
-		//Draw lives
-		g.setColor(Color.red);
-		g.setFont(new Font("TimesRoman", Font.PLAIN, 24));
-		g.drawString("LIVES: "+lives, WIDTH-125, 25);
-		
-		//Draw the Circle
-		g.setColor(Color.white);
-		//g.drawOval(WIDTH-550, 25, 500, 500);
-		g.drawOval((WIDTH / 2) - c_size/2, (HEIGHT/2) - c_size/2, c_size, c_size);
-		
-		//Draw the Player
-		g.drawImage(player.getSprite(), player.getRotation(), player.getX() - (player.getSprite().getWidth()/2), player.getY() - (player.getSprite().getHeight()/2));
-		g.setColor(player.getColor());
-		g.fillOval(player.getX() - (player.getSize()/2), player.getY() - (player.getSize()/2), player.getSize(), player.getSize());
 
-		
-		//Draw the enemies
-		for(Enemy e:enemies){
-			e.draw(g);
-		}
-		
-		
-		//******************************************************************
-		// Sound and Audio
-		//******************************************************************
+
+			//******************************************************************
+			// Sound and Audio
+			//******************************************************************
 		}else{
 			g.setColor(Color.red);
 			g.setFont(new Font("TimesRoman", Font.PLAIN, 124));
 			g.drawString("GAME OVER", WIDTH/2-370, HEIGHT/2);
 		}
 	}
-	
+
 	/**
 	 * Checks and returns if the game should be ended
 	 * @return
@@ -132,7 +152,7 @@ public class CircleShooter extends Game{
 	public boolean checkEndGame(){
 		return lives==0;
 	}
-	
+
 	public static void main(String[] args){
 		Arcadia.display(new Arcadia(new CircleShooter()));
 	}
